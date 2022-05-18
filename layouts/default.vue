a477790d2c5efe65bdf8748999cb6847a661e139
Kenan Kadic <ken.kadic@outlook.com>
2022-05-18 10:01:31 -0500

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-app dark>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>Vuetify Dashboard</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text rounded to="/">Home</v-btn>
+      <v-btn text rounded to="/login">Login</v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <Nuxt />
+    </v-main>
+
+    <v-footer
+      color="primary lighten-1"
+      padless
+    >
+      <v-row
+        justify="center"
+        no-gutters
+      >
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          color="white"
+          text
+          rounded
+          class="my-2"
+        >
+          {{ link }}
+        </v-btn>
+        <v-col
+          class="primary lighten-2 py-4 text-center white--text"
+          cols="12"
+        >
+          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        </v-col>
+      </v-row>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data () {
+    return {
+      links: [
+        {label: 'Home', url:'/'},
+        {label: 'Login', url:'login'}
+      ],
+    }
+  }
+}
+</script>
