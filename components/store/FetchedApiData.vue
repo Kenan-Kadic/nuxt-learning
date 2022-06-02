20efd4d66e06fdd8dc8a7940514e07b42940f409
Kenan Kadic <ken.kadic@outlook.com>
2022-06-02 13:17:30 -0500

    fetch

--- components/store/FetchedApiData.vue
+++ components/store/FetchedApiData.vue
@@ -1,11 +1,13 @@
 <template>
   <v-container>
-    <h1>Fetch Users and Save to Store</h1>
-    <v-btn class="green" @click="fetchSomething">Fetch API User DATA</v-btn>
+    <h1>Fetch User Data via API and save data to Vuex Store right away</h1>
+    <v-btn class="green my-5" @click="fetchSomething">Fetch API User DATA</v-btn>
 
-    <h2>Display User Data (retrieved from store)</h2>
-    <div v-for="(user, index) in usersFromStore" :key="index">
-    <p>{{ user.id }}</p>
+    <h1>Display User Data (retrieved from Vuex store after Fetch) for Each User in User Data Object</h1>
+    <div class="my-5" v-for="(user, index) in usersFromStore" :key="index">
+    <h1>User Id: {{ user.id }}</h1>
+    <h2 class>User City: {{ user.address.city}}</h2>
+    <h2 class>Website: {{ user.website}}</h2>
      </div>
   </v-container>
 </template>
@@ -26,8 +28,8 @@ export default {
       this.$axios.get('https://jsonplaceholder.typicode.com/users')
         .then(resp => {
           if (resp.status === 200) {
-            alert(resp.data);
             this.users = resp.data;
+            console.log(this.users)
             this.$store.commit("store/CHANGE_DATA", this.users);
           }
         })
@@ -39,8 +41,7 @@ export default {
 
   computed: {
     usersFromStore() {
-      console.log(this.$store.state["store/data"])
-      return this.$store.state["store/data"];
+      return this.$store.state.store.data
     },
   },
 }
